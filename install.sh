714a2cd4f42f8330450f98764cce230ce23e18fc
Georman <georman.calderon@cleteci.com>
2024-06-12 16:54:01 -0400

    fix install

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir /var/www/html/my-react-app
 sudo touch /etc/nginx/conf.d/react.conf
 sudo cp react.conf /etc/nginx/conf.d/react.conf
-mkdir my-app
-cd my-app
-git clone https://github.com/georve/team-presentation.git
-cd team-presentation/
+#mkdir my-app
+#cd my-app
+#git clone https://github.com/georve/team-presentation.git
+#cd team-presentation/
 npm run build
 sudo cp -R dist/ /var/www/html/my-react-app/
 sudo nginx -t && sudo systemctl reload nginx
