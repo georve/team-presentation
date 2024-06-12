f6b642cea278bfc6759115a9644225c107c93334
Jocelyn <jocelyn.cf@gmail.com>
2024-06-12 12:45:32 +0200

    correccion de estilos

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
+
 const Teams = () => {
   const teamMembers = [
     {
@@ -9,15 +10,15 @@ const Teams = () => {
         'https://res.cloudinary.com/pruebaweb/image/upload/v1718181542/erika_eyrixf.jpg',
       name: 'Erika Patricia Montoya',
       pais: 'Peru',
-      hobby: '',
+      hobby: 'aprender cosas nuevas',
       delay: 0.1,
     },
     {
       id: 2,
       imgSrc: 'https://res.cloudinary.com/pruebaweb/image/upload/v1696916616/IMG_4131_ebn4ka.jpg',
       name: 'Jocelyn Castro',
-      pais: 'Espana',
-      hobby: '',
+      pais: 'Chile',
+      hobby: 'Programar',
       delay: 0.3,
     },
     {
@@ -25,7 +26,7 @@ const Teams = () => {
       imgSrc: 'https://res.cloudinary.com/pruebaweb/image/upload/v1718181808/Mio_xu15hg.jpg',
       name: 'Mio Ogura',
       pais: 'Japon',
-      hobby: '',
+      hobby: 'Ir a conciertos',
       delay: 0.5,
     },
     {
@@ -34,15 +35,15 @@ const Teams = () => {
         'https://res.cloudinary.com/pruebaweb/image/upload/v1718181793/eva_qdqcuy.png',
       name: 'Eva Garcia',
       pais: 'Espana',
-      hobby: '',
+      hobby: 'Autoaprendizaje de tecnologias',
       delay: 0.1,
     },
     {
       id: 5,
       imgSrc: 'https://res.cloudinary.com/pruebaweb/image/upload/v1718181859/belen_oycys1.jpg',
       name: 'Belen Aguirre',
-      pais: 'Espana',
-      hobby: '',
+      pais: 'Argentina',
+      hobby: 'Aprender',
       delay: 0.3,
     },
     {
@@ -50,7 +51,7 @@ const Teams = () => {
       imgSrc: 'https://res.cloudinary.com/pruebaweb/image/upload/v1718182006/Nikoo_dikvtj.jpg',
       name: 'Niko Salehi',
       pais: 'Iran',
-      hobby: '',
+      hobby: 'Viajar',
       delay: 0.5,
     },
     {
@@ -58,21 +59,21 @@ const Teams = () => {
       imgSrc: 'https://res.cloudinary.com/pruebaweb/image/upload/v1718182084/georman_zntwpi.jpg',
       name: 'Georman Calderon',
       pais: 'Venezuela',
-      hobby: '',
+      hobby: 'Senderismo',
       delay: 0.5,
     },
   ];
 
   return (
-    <div className="container-xxl py-6 pb-5" id="team">
+    <div className="container-xxl py-6 pb-5 bg-gray-800" id="team">
       <div className="container">
         <div className="row g-5 mb-5 wow fadeInUp" data-wow-delay="0.1s">
           <div className="col-lg-6">
-            <h1 className="display-5 mb-0">Team Members</h1>
+            <h1 className="display-5 mb-0 text-slate-300">Team Clouders 6</h1>
           </div>
           <div className="col-lg-6 text-lg-end">
-            <a className="btn btn-primary py-3 px-5" href="">
-              Contact Us
+            <a className="btn btn-primary py-3 px-5" href="https://github.com/georve/team-presentation">
+              Github
             </a>
           </div>
         </div>
@@ -84,12 +85,12 @@ const Teams = () => {
               data-wow-delay={member.delay}
             >
               <div className="team-item position-relative">
-                <img className="img-fluid rounded h-4 w-4" src={member.imgSrc} alt="" />
+                <img className="img-fluid rounded " src={member.imgSrc} alt="" style={{ width: '300px', height: '320px' }} />
                 <div className="team-text bg-white rounded-end p-4">
                   <div>
                     <h5>{member.name}</h5>
                     <span>{member.pais}</span>
-                    <span>{member.hobby}</span>
+                    <span className='m-2'>{member.hobby}</span>
                   </div>
                   <FaArrowRight className="text-primary" size={40} />
                 </div>
